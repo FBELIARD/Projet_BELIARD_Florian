e56095c589e9205c3d5afa3fb1e735f9bf2b775f
FBELIARD <84351903+FBELIARD@users.noreply.github.com>
2021-05-30 13:47:51 +0200

    Add files via upload
    Upload code

---
+++ src/main/java/com/example/final_project_beliard/presentation/list/Country.kt
@@ -0,0 +1,6 @@
+package com.example.final_project_beliard.presentation.list
+
+data class Country (
+    val name: String,
+    val url: String
+)
---
+++ src/main/java/com/example/final_project_beliard/presentation/list/CountryAdapter.kt
@@ -0,0 +1,66 @@
+package com.example.final_project_beliard.presentation.list
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.final_project_beliard.R
+
+class CountryAdapter(private var dataSet: List<Country>, var listener :((Int) -> Unit)? = null) : RecyclerView.Adapter<CountryAdapter.ViewHolder>() {
+
+
+    /**
+     * Provide a reference to the type of views that you are using
+     * (custom ViewHolder).
+     */
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val textView: TextView
+        val imageView: ImageView
+
+        init {
+            // Define click listener for the ViewHolder's View.
+            textView = view.findViewById(R.id.country_name)
+            imageView = view.findViewById(R.id.pokemon_img)
+        }
+    }
+
+    fun updateList(list: List<Country>) {
+        dataSet = list
+        notifyDataSetChanged()
+    }
+
+    // Create new views (invoked by the layout manager)
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+        // Create a new view, which defines the UI of the list item
+        val view = LayoutInflater.from(viewGroup.context)
+            .inflate(R.layout.country_item, viewGroup, false)
+
+        return ViewHolder(view)
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+
+        // Get element from your dataset at this position and replace the
+        // contents of the view with that element
+        val country : Country = dataSet[position]
+        viewHolder.textView.text = country.name
+        viewHolder.itemView.setOnClickListener {
+            listener?.invoke(position)
+        }
+
+        Glide
+            .with(viewHolder.itemView.context)
+            .load("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${position + 1}.png")
+            .centerCrop()
+            .into(viewHolder.imageView)
+
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    override fun getItemCount() = dataSet.size
+
+}
